2b6ca48c250437527b6fc2dc71927879f9785024
matlab22 <46869303+matlab22@users.noreply.github.com>
2026-02-22 23:06:11 +0100

    Add transition support via command string parameter

--- zigbee_led.php
+++ zigbee_led.php
@@ -17,16 +17,29 @@
 	list($topic, $data ) = explode( '#', $commandline, 2);
 	// ----------------------------------
 	
-	list($command, $value_pct) = explode( ' ', $data);
+	$parts = explode( ' ', $data);
+	$command = $parts[0];
+	$value_pct = $parts[1];
 	
 	$data = array ( );
 
-	// checks if the command comes with costum max / min levels
-	if (preg_match('/^(white|rgb|rgbw|tunablew)(?:_(\d+)_(\d+))?(?:_(\d+))?$/', $command, $matches)) {
+	// checks if the command comes with optional max / min levels and optional transition
+	// white/rgb: (command)[_(bright_max)][_(transition)]
+	// rgbw/tunablew: (command)[_(ctemp_max)_(ctemp_min)][_(bright_max)][_(transition)]
+	$ctemp_max = 500;  # default 6500K
+	$ctemp_min = 150;  # default 2700K
+	$bright_max = 254; # default 100%
+	$transition = 0;
+	if (preg_match('/^(white|rgb)(?:_(\d+))?(?:_([\d.]+))?$/', $command, $matches)) {
 		$command = $matches[1];
-		$ctemp_max = isset($matches[2]) ? intval($matches[2]) : 500;#6500;
-		$ctemp_min = isset($matches[3]) ? intval($matches[3]) : 150;#3000;
-		$bright_max = isset($matches[4]) ? intval($matches[4]) : 254;#100;
+		$bright_max = isset($matches[2]) && $matches[2] !== '' ? intval($matches[2]) : 254;
+		$transition = isset($matches[3]) && $matches[3] !== '' ? floatval($matches[3]) : 0;
+	} elseif (preg_match('/^(rgbw|tunablew)(?:_(\d+)_(\d+))?(?:_(\d+))?(?:_([\d.]+))?$/', $command, $matches)) {
+		$command = $matches[1];
+		$ctemp_max = isset($matches[2]) && $matches[2] !== '' ? intval($matches[2]) : 500;
+		$ctemp_min = isset($matches[3]) && $matches[3] !== '' ? intval($matches[3]) : 150;
+		$bright_max = isset($matches[4]) && $matches[4] !== '' ? intval($matches[4]) : 254;
+		$transition = isset($matches[5]) && $matches[5] !== '' ? floatval($matches[5]) : 0;
 	}
 
 	switch ($command) {
@@ -109,5 +122,9 @@
 			error_log('Transformer shelly_rgb&w: Wrong parameters (white or rgb missing)');
 	}
 	
+	if ($transition !== null) {
+		$data['transition'] = $transition;
+	}
+
 	echo $topic."#".json_encode($data, JSON_UNESCAPED_SLASHES)."\n";
 	
